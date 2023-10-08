eff2b82c3f3bf98a2c94c55683b66ae8f5528eb2
Ktag4g <64621918+Ktag4g@users.noreply.github.com>
2023-10-08 13:53:59 -0400

    Fixed one-time collision
    Collision code was triggering multiple times during a collision
    
    Fixed it so it only counts the collision once

--- Assets/Scripts/CollideDetect.cs
+++ Assets/Scripts/CollideDetect.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CollideDetect : MonoBehaviour
 {
     private int BearCounter = 0;
+    private bool collisionOccured = false;
 
     public Rigidbody rb;
 
@@ -14,35 +15,60 @@ public class CollideDetect : MonoBehaviour
         //Finding Player gameobject by name
         Player = GameObject.Find("Player");
 
+        //Resets bear physics
         rb = GetComponent<Rigidbody>();
-
         rb.inertiaTensor = new Vector3(1, 1, 1);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Obstacle")
+        //Checks to see if collision has already haappened
+        if (collisionOccured == false)
         {
-            if (BearCounter > 0)
+            if (other.gameObject.tag == "Obstacle")
             {
-                BearCounter = BearCounter - 1;
+                if (BearCounter > 0)
+                {
+                    //Knocks off a bear when hit by an asteroid
+                    BearCounter = BearCounter - 1;
+                    Debug.Log("Bear Counter: " + BearCounter);
+
+                    //Confirms collision
+                    collisionOccured = true;
+                }
+                else
+                {
+                    //Ends the game when out of bears
+                    Debug.Log("Game Over!");
+
+                    //Launches bear
+                    rb.velocity = new Vector3(3, 3, 0);
+                    
+                    //Confirms collision
+                    collisionOccured = true;
+                }
+            }
+            else if (other.gameObject.tag == "Bear")
+            {
+                //Resets bear physics
+                rb.velocity = new Vector3(0, 0, 0);
+                rb.angularVelocity = new Vector3(0, 0, 0);
+
+                //Collects bear, updates score
+                Destroy(other.gameObject);
+                BearCounter = BearCounter + 1;
                 Debug.Log("Bear Counter: " + BearCounter);
-            }
-            else
-            {
-                Debug.Log("Game Over!");
 
-                rb.velocity = new Vector3(3, 3, 0);
+                //Confirms collision
+                collisionOccured = true;
             }
-        }
-        else if (other.gameObject.tag == "Bear")
-        {
-            rb.velocity = new Vector3(0, 0, 0);
-            rb.angularVelocity = new Vector3(0, 0, 0);
-
-            Destroy(other.gameObject);
-            BearCounter = BearCounter + 1;
-            Debug.Log("Bear Counter: " + BearCounter);
         }
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        collisionOccured = false;
+    }
+
+
 }
